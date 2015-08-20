abcb26c5c2337097d7bb3656d6b24a93042cf506
fumio_shimamura <sun.shimamura@gmail.com>
2015-08-20 14:12:05 +0000

    marge ranking

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,7 +1,4 @@
 class RankingController < ApplicationController
-  after_action :set_rank
-  
-  @rank = []
 
   def have
     @i = 1
@@ -40,32 +37,6 @@ class RankingController < ApplicationController
     
     @type = 'Want'
     @rank = Ownership.group("item_id").where("type = 'Want'").order("count_all DESC").limit(10).count
-
-    @rank.each do |item_id, count|
-      # item_idを元に@item情報を取得してセットする
-      @item = Item.find(item_id)
-      @rank_items.push(@item)
-      # countからHave/Wantの数を取得してセットする
-      @rank_counts.push(count)
-      # 順位をセットする。同数のHave/Wantの場合、同じ順位にする。
-      if @save_count != count
-        @ranking = @i
-      end
-      @rank_rankings.push(@ranking)
-      @i = @i + 1
-      @save_count = count
-    end
-  end
-  
-  private
-  
-  def set_rank
-    @i = 1
-    @ranking = 1
-    @save_count = 0
-    @rank_items = []
-    @rank_counts = []
-    @rank_rankings = []
 
     @rank.each do |item_id, count|
       # item_idを元に@item情報を取得してセットする
